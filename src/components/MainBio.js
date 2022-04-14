948e3a3a3f050df79452abf23de47f95ec45335f
Eileen Mitchell <eileenhlmitchell19@gmail.com>
2022-04-13 17:57:44 -0700

    get up to date

--- src/components/MainBio.js
+++ src/components/MainBio.js
@@ -1,3 +1,5 @@
+import aboutMeimg from '../IMG_0109.jpeg';
+
 function MainBio(){
 return (
     
@@ -5,12 +7,11 @@ return (
     <div className="container">
     <div className="row">
       <div className="col-auto">
-        <img src="./IMG_0109.jpeg" alt="kitten"></img>
+        <img className="photo1" src={aboutMeimg} alt="kitten"></img>
         <div className="col">
         <h1>Eileen Mitchell</h1>
-        <p>About Me</p>
         <p>
-            My name is Eileen Mitchell. I am from Visalia, CA and graduated with a BS in City and Regional Planning, Minor in Political Science and Certificate in Geographic Information Systems (GIS) from Fresno State University.
+            I am from Visalia, CA and graduated with a BS in City and Regional Planning, Minor in Political Science and Certificate in Geographic Information Systems (GIS) from Fresno State University.
         </p>
         <p>
             I recently began a new coding bootcamp through the University of Washington in Fullstack Development and now reside in Seattle, WA working as an Associate Planner for a civil engineering firm. I love what I do, but have been intrigued by the world of tech and am broadening my knowledge in this realm. One similarity I have found  between both of these fields is the need to look for solutions, wherever they might be. I find this exciting, and am continuing to expand my knowledge through various projects from front end HTML, CSS, Javascript and JQuery, to backend SQL, Mongo DB, Mongoose, GraphQL, React. One thing I am learning is that I am just scraping the surface of what there is to know, but I cant wait to dive in and continue learning and expanding my knowledge.
